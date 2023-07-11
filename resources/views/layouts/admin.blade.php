37ec8654bc52597bbd365ef8905475ee70719c37
LettyCoach <laneandyumiko@gmail.com>
2023-07-12 06:32:29 +0900

    fix(invoice app)

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,12 +14,14 @@
 
     <!-- Fonts -->
     <link href="{{ asset('public/css/lib/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('public/css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
-    <script src="{{asset('public/js/easy-number-separator.js')}}"></script>
+    <script src="{{ asset('public/js/easy-number-separator.js') }}"></script>
 
 
 </head>
@@ -29,11 +31,13 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" style="background: #3A00FF;">
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar"
+            style="background: #3A00FF;">
 
             <!-- Sidebar - Brand -->
             <a class="align-items-center justify-content-center text-decoration-none" href="{{ asset('/') }}">
-                <img src="{{ asset('public/img/quanto_logo.png') }}" class="m-2" style="width: 100%; margin: 0!important; padding: .5rem;">
+                <img src="{{ asset('public/img/quanto_logo.png') }}" class="m-2"
+                    style="width: 100%; margin: 0!important; padding: .5rem;">
                 <p class="text-white text-center mr-4">クライアント質問管理</p>
             </a>
 
@@ -68,10 +72,10 @@
             </li>
 
             <hr class="sidebar-divider">
-            <li class="nav-item {{ Nav::isRoute('admin.userProducts') }}">
+            <li class="nav-item {{ Nav::isRoute('admin.userProducts') }} {{ Nav::isRoute('admin.userProductCategories') }}">
                 <a class="nav-link" href="{{ route('admin.userProducts') }}">
-                        <i class="fas fa-book"></i>
-                        <span>{{ __('商品管理') }}</span>
+                    <i class="fas fa-book"></i>
+                    <span>{{ __('商品管理') }}</span>
                 </a>
                 <ul class="" id="">
                     <li>
@@ -91,10 +95,10 @@
                     <span>{{ __('関連データ管理') }}</span></a>
             </li>
             <!-- フォーム作成 -->
-            <li class="nav-item {{ Nav::isRoute('admin.users') }}">
-                <a class="nav-link" href="{{ route('admin.users') }}">
-                        <i class="fas fa-book"></i>
-                        <span>{{ __('フォーム作成') }}</span>
+            <li class="nav-item {{ Nav::isRoute('paper.invoice') }}">
+                <a class="nav-link" href="{{ route('paper.invoice') }}">
+                    <i class="fas fa-book"></i>
+                    <span>{{ __('フォーム作成') }}</span>
                 </a>
                 <ul class="" id="">
                     <li>
@@ -124,7 +128,7 @@
                 </ul>
             </li>
             <!-- クライアント管理 -->
-            <li class="nav-item {{ Nav::isRoute('admin.users') }}">
+            <li class="nav-item {{ Nav::isRoute('admin.users') }}  {{ Nav::isRoute('admin.user.create') }}">
                 <a class="nav-link" href="{{ route('admin.users') }}">
                     <i class="fas fa-user-friends"></i>
                     <span>{{ __('クライアント管理') }}</span>
@@ -133,16 +137,14 @@
                     <li>
                         <a class="nav-link" href="{{ route('admin.user.create') }}">
                             <i class="fas fa-user-friends"></i>
-                        <span>{{ __('新規登録') }}</span></a>
+                            <span>{{ __('新規登録') }}</span></a>
                     </li>
                 </ul>
             </li>
 
             @if (Auth::check())
-            @if (Auth::user()->isAdmin())
-
-
-            @endif
+                @if (Auth::user()->isAdmin())
+                @endif
             @endif
 
 
@@ -193,14 +195,18 @@
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
                         <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
                             </a>
                             <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                                aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <input type="text" class="form-control bg-light border-0 small"
+                                            placeholder="Search for..." aria-label="Search"
+                                            aria-describedby="basic-addon2">
                                         <div class="input-group-append">
                                             <button class="btn btn-primary" type="button">
                                                 <i class="fas fa-search fa-sm"></i>
@@ -213,16 +219,20 @@
 
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                                <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->name[0] }}">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span
+                                    class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+                                <figure class="img-profile rounded-circle avatar font-weight-bold"
+                                    data-initial="{{ Auth::user()->name[0] }}">
                                     @if (Auth::user()->profile_url != null)
-                                    <img src="{{url(Auth::user()->profile_url)}}">
+                                        <img src="{{ url(Auth::user()->profile_url) }}">
                                     @endif
                                 </figure>
                             </a>
                             <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="{{ route('profile') }}">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     {{ __('Profile') }}
@@ -232,7 +242,8 @@
                                     {{ __('Settings') }}
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" href="#" data-toggle="modal"
+                                    data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     {{ __('ログアウト') }}
                                 </a>
@@ -276,7 +287,8 @@
     </a>
 
     <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -288,7 +300,8 @@
                 <div class="modal-body">現在のセッションを終了する場合は、以下のログアウトボタンを押してください。</div>
                 <div class="modal-footer">
                     <button class="btn btn-link" type="button" data-dismiss="modal">{{ __('キャンセル') }}</button>
-                    <a class="btn btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('ログアウト') }}</a>
+                    <a class="btn btn-danger" href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('ログアウト') }}</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
@@ -297,15 +310,15 @@
         </div>
     </div>
 
-<!-- Scripts -->
-<script src="{{ asset('public/js/lib/jquery.min.js') }}"></script>
-<script src="{{ asset('public/js/lib/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
-<script src="{{ asset('public/js/lib/jquery.easing.min.js') }}"></script>
-<script src="{{ asset('public/js/sb-admin-2.min.js') }}"></script>
-<script src="{{ asset('public/js/admin.js') }}"></script>
-<script src="{{ asset('public/js/drag.js') }}"></script>
-@yield('js')
+    <!-- Scripts -->
+    <script src="{{ asset('public/js/lib/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/js/lib/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
+    <script src="{{ asset('public/js/lib/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('public/js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ asset('public/js/admin.js') }}"></script>
+    <script src="{{ asset('public/js/drag.js') }}"></script>
+    @yield('js')
 </body>
 
 </html>
